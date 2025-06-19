dd67b5c1e229672dfd7866c61c1eb31376c4fe3c
harish khileri <harishkhilery98@gmail.com>
2025-06-19 14:49:52 +0530

    change the base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/lovable-app/friendly-feed-app", 
+  base: "/friendly-feed-app", 
   server: {
     host: "::",
     port: 8080,
